2275ceda7861359dcbb6f3faf77a357a28408199
oktayshakirov <oktayshakirov@gmail.com>
2024-11-01 03:10:32 +0100

    added obstacles, updated player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,39 +4,99 @@ using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour {
 
     bool alive = true;
-
     public float speed = 5;
     [SerializeField] Rigidbody rb;
 
-    float horizontalInput;
-    [SerializeField] float horizontalMultiplier = 2;
+    private int currentLane = 1;
+    private float laneDistance = 3.0f;
+    private bool isMoving = false;
 
     public float speedIncreasePerPoint = 0.1f;
 
-    private void FixedUpdate ()
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+    private float swipeThreshold = 50f;
+
+    private void FixedUpdate()
     {
         if (!alive) return;
 
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
-        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+        rb.MovePosition(rb.position + forwardMove);
+
+        Vector3 targetPosition = new Vector3((currentLane - 1) * laneDistance, rb.position.y, rb.position.z);
+        rb.MovePosition(Vector3.Lerp(rb.position, targetPosition, Time.fixedDeltaTime * 10));
     }
 
-    private void Update () {
-        horizontalInput = Input.GetAxis("Horizontal");
-
+    private void Update()
+    {
         if (transform.position.y < -5) {
             Die();
         }
-	}
 
-    public void Die ()
+        DetectInput();
+    }
+
+    private void DetectInput()
+    {
+        if (!isMoving)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLane > 0)
+            {
+                currentLane--;
+                isMoving = true;
+                Invoke("ResetMove", 0.1f);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && currentLane < 2)
+            {
+                currentLane++;
+                isMoving = true;
+                Invoke("ResetMove", 0.1f);
+            }
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    startTouchPosition = touch.position;
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    endTouchPosition = touch.position;
+                    Vector2 swipe = endTouchPosition - startTouchPosition;
+
+                    if (Mathf.Abs(swipe.x) > swipeThreshold)
+                    {
+                        if (swipe.x > 0 && currentLane < 2)
+                        {
+                            currentLane++;
+                        }
+                        else if (swipe.x < 0 && currentLane > 0)
+                        {
+                            currentLane--;
+                        }
+                        isMoving = true;
+                        Invoke("ResetMove", 0.1f);
+                    }
+                }
+            }
+        }
+    }
+
+    void ResetMove()
+    {
+        isMoving = false;
+    }
+
+    public void Die()
     {
         alive = false;
         Invoke("Restart", 2);
     }
 
-    void Restart ()
+    void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
