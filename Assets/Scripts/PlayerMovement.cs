0cd821f3e69aec9c697ef1df760abe96c3460406
Oktay Shakirov <oktayshakirov@gmail.com>
2024-11-17 17:00:57 +0100

    added ramps for jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,8 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
     bool alive = true;
     public float speed = 5;
     [SerializeField] Rigidbody rb;
@@ -27,39 +27,62 @@ public class PlayerMovement : MonoBehaviour {
     public float shakeAmount = 0.05f;
     private Quaternion targetRotation;
 
-    private void Start() {
+    public float rampSpeedMultiplier = 1.2f;
+    private bool onRamp = false;
+    private Vector3 rampNormal;
+
+    private void Start()
+    {
         targetRotation = transform.rotation;
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         if (!alive) return;
 
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+
+        if (onRamp)
+        {
+            forwardMove += Vector3.down * 0.1f; // Simulate gravity on the ramp
+            rb.rotation = Quaternion.Lerp(rb.rotation, Quaternion.LookRotation(Vector3.Cross(transform.right, rampNormal)), Time.fixedDeltaTime * leanSpeed);
+        }
+
         rb.MovePosition(rb.position + forwardMove);
 
         Vector3 targetPosition = new Vector3((currentLane - 1) * laneDistance, rb.position.y, rb.position.z);
         rb.MovePosition(Vector3.Lerp(rb.position, targetPosition, Time.fixedDeltaTime * 10));
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.fixedDeltaTime * leanSpeed);
+        if (!onRamp)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.fixedDeltaTime * leanSpeed);
+        }
     }
 
-    private void Update() {
-        if (transform.position.y < -5) {
+    private void Update()
+    {
+        if (transform.position.y < -5)
+        {
             Die();
         }
 
         DetectInput();
     }
 
-    private void DetectInput() {
-        if (!isMoving) {
-            if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLane > 0) {
+    private void DetectInput()
+    {
+        if (!isMoving)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLane > 0)
+            {
                 currentLane--;
                 ApplyLean(-leanAngle);
                 RotateHands(-handRotationAngle);
                 isMoving = true;
                 Invoke("ResetMove", 0.1f);
-            } else if (Input.GetKeyDown(KeyCode.RightArrow) && currentLane < 2) {
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && currentLane < 2)
+            {
                 currentLane++;
                 ApplyLean(leanAngle);
                 RotateHands(handRotationAngle);
@@ -67,21 +90,29 @@ public class PlayerMovement : MonoBehaviour {
                 Invoke("ResetMove", 0.1f);
             }
 
-            if (Input.touchCount > 0) {
+            if (Input.touchCount > 0)
+            {
                 Touch touch = Input.GetTouch(0);
 
-                if (touch.phase == TouchPhase.Began) {
+                if (touch.phase == TouchPhase.Began)
+                {
                     startTouchPosition = touch.position;
-                } else if (touch.phase == TouchPhase.Ended) {
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
                     endTouchPosition = touch.position;
                     Vector2 swipe = endTouchPosition - startTouchPosition;
 
-                    if (Mathf.Abs(swipe.x) > swipeThreshold) {
-                        if (swipe.x > 0 && currentLane < 2) {
+                    if (Mathf.Abs(swipe.x) > swipeThreshold)
+                    {
+                        if (swipe.x > 0 && currentLane < 2)
+                        {
                             currentLane++;
                             ApplyLean(leanAngle);
                             RotateHands(handRotationAngle);
-                        } else if (swipe.x < 0 && currentLane > 0) {
+                        }
+                        else if (swipe.x < 0 && currentLane > 0)
+                        {
                             currentLane--;
                             ApplyLean(-leanAngle);
                             RotateHands(-handRotationAngle);
@@ -94,32 +125,40 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    private void ApplyLean(float angle) {
+    private void ApplyLean(float angle)
+    {
         targetRotation = Quaternion.Euler(0, 0, angle);
     }
 
-    private void RotateHands(float angle) {
-        if (angle > 0) {
+    private void RotateHands(float angle)
+    {
+        if (angle > 0)
+        {
             leftHand.localRotation = Quaternion.Euler(0, 0, -handRotationAngle);
             rightHand.localRotation = Quaternion.Euler(0, 0, handRotationAngle);
-        } else {
+        }
+        else
+        {
             leftHand.localRotation = Quaternion.Euler(0, 0, handRotationAngle);
             rightHand.localRotation = Quaternion.Euler(0, 0, -handRotationAngle);
         }
     }
 
-    private void ResetMove() {
+    private void ResetMove()
+    {
         isMoving = false;
         targetRotation = Quaternion.Euler(0, 0, 0);
         StartCoroutine(ResetHands());
     }
 
-    private IEnumerator ResetHands() {
+    private IEnumerator ResetHands()
+    {
         Quaternion leftHandStartRotation = leftHand.localRotation;
         Quaternion rightHandStartRotation = rightHand.localRotation;
         float elapsedTime = 0f;
 
-        while (elapsedTime < 1f) {
+        while (elapsedTime < 1f)
+        {
             leftHand.localRotation = Quaternion.Lerp(leftHandStartRotation, Quaternion.identity, elapsedTime);
             rightHand.localRotation = Quaternion.Lerp(rightHandStartRotation, Quaternion.identity, elapsedTime);
             elapsedTime += Time.deltaTime * handRotationSpeed;
@@ -130,12 +169,33 @@ public class PlayerMovement : MonoBehaviour {
         rightHand.localRotation = Quaternion.identity;
     }
 
-    public void Die() {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ramp"))
+        {
+            onRamp = true;
+            rampNormal = collision.contacts[0].normal;
+            speed *= rampSpeedMultiplier;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ramp"))
+        {
+            onRamp = false;
+            speed /= rampSpeedMultiplier;
+        }
+    }
+
+    public void Die()
+    {
         alive = false;
         Invoke("Restart", 2);
     }
 
-    private void Restart() {
+    private void Restart()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
