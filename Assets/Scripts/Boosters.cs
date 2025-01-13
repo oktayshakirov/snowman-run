15b08baceb71ed1d4f033498d8d7bf8943d09dc7
Oktay Shakirov <oktayshakirov@gmail.com>
2025-01-13 20:27:03 +0100

    added timers

--- Assets/Scripts/Boosters.cs
+++ Assets/Scripts/Boosters.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class Boosters : MonoBehaviour
 {
     [Header("Booster Settings")]
-    [SerializeField] private float gogglesFogReduction = 0.8f;
-    [SerializeField] private float gogglesDuration = 7f;
+    [SerializeField] private float gogglesFogReduction = 0.5f;
+    [SerializeField] private float gogglesDuration = 5f;
     public float GogglesDuration => gogglesDuration;
 
+    [Header("Timer Settings")]
+    [SerializeField] private Timer circleTimer;
     private bool gogglesActive = false;
     private float fogDensityAtActivation;
     private Fog Fog => Fog.Instance;
+
+    private void Start()
+    {
+        if (circleTimer != null)
+        {
+            circleTimer.gameObject.SetActive(false);
+            circleTimer.hours = 0;
+            circleTimer.minutes = 0;
+            circleTimer.seconds = (int)gogglesDuration;
+            circleTimer.countMethod = Timer.CountMethod.CountDown;
+            circleTimer.outputType = Timer.OutputType.StandardText;
+        }
+    }
 
     public void ActivateGoggles()
     {
@@ -19,15 +34,48 @@ public class Boosters : MonoBehaviour
             return;
         }
         gogglesActive = true;
+
+        if (circleTimer != null)
+        {
+            circleTimer.gameObject.SetActive(true);
+            circleTimer.StartTimer();
+            circleTimer.onTimerEnd.AddListener(OnTimerEnd);
+        }
+
         Fog.HoldFogIncrement(true);
         fogDensityAtActivation = Fog.GetCurrentFogDensity();
         Fog.ReduceFogDensity(gogglesFogReduction);
         StartCoroutine(GogglesCooldown());
     }
 
+    private void OnTimerEnd()
+    {
+        Debug.Log("Timer finished!");
+    }
+
     private IEnumerator GogglesCooldown()
     {
-        yield return new WaitForSeconds(gogglesDuration);
+        float remainingTime = gogglesDuration;
+
+        while (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+
+            if (circleTimer != null)
+            {
+                circleTimer.timeRemaining = remainingTime;
+                circleTimer.DisplayInTextObject();
+            }
+
+            yield return null;
+        }
+
+        if (circleTimer != null)
+        {
+            circleTimer.StopTimer();
+            circleTimer.gameObject.SetActive(false);
+        }
+
         Fog.RestoreFogDensityTo(fogDensityAtActivation);
         Fog.HoldFogIncrement(false);
         gogglesActive = false;
